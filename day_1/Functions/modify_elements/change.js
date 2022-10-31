edb7e3deb4593331dc6aaba0a662e5636ab2be57
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-10-31 04:46:02 +0100

    Removing items from DOM

---
+++ day_1/Functions/modify_elements/change.js
@@ -0,0 +1,9 @@
+// Changing the background color of the body element to red
+test1 = document.querySelector('body');
+test1.style.backgroundColor = 'red';
+// Changing the background color of the first p element to blue
+test2 = document.querySelector('p');
+test2.style.backgroundColor = 'blue';
+// Changing the background color of the first h1 element to green
+test3 = document.querySelector('h1');
+test3.style.backgroundColor = 'green';
---
+++ day_1/Functions/modify_elements/remove.js
@@ -0,0 +1,28 @@
+// FIRST ADD ELEMENTS TO THE DOM
+
+test1 = document.getElementById('testing-area')
+test1.innerHTML = 'Hello World'
+
+test2 = document.createElement('p')
+test2.innerHTML = 'This is the first addition'
+
+test3 = document.createElement('p')
+test3.innerHTML = 'This is another addition'
+
+test4 = document.createElement('p')
+test4.innerHTML = 'Note to self, learn Flexbox'
+
+
+test1.appendChild(test2)
+test1.appendChild(test3)
+test1.appendChild(test4)
+
+
+// NOW REMOVE ELEMENTS FROM THE DOM
+
+test1.removeChild(test2)
+test1.removeChild(test3)
+test1.removeChild(test4)
+
+
+
