bd179c19d552537ea8a6d48946ac70ed5e2deede
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-01-31 09:50:49 +0100

    C:/Program Files/Git/about page structure

---
+++ lykdat-ecommerce-clone/lykdat/components/Partners.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Partners = () => {
+	return ( 
+		<Box>
+			<Text as='b' fontSize='2xl'>Our Brand Partners</Text>
+			<Text> Map Partners Logos here </Text>
+		</Box>
+	 );
+}
+ 
+export default Partners;
--- lykdat-ecommerce-clone/lykdat/pages/about.tsx
+++ lykdat-ecommerce-clone/lykdat/pages/about.tsx
@@ -1,5 +1,11 @@
 import AboutHero from "<lykdat>/components/AboutHero";
+import Feedback from "<lykdat>/components/Feedback";
+import Footer from "<lykdat>/components/Footer";
 import Navbar from "<lykdat>/components/Navbar";
+import Partners from "<lykdat>/components/Partners";
+import Story from "<lykdat>/components/Story";
+import Team from "<lykdat>/components/Team";
+import { Box } from "@chakra-ui/react";
 import Head from "next/head";
 const About = () => {
 	return ( 
@@ -10,8 +16,15 @@ const About = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       	</Head>
+		<Box>
 		<Navbar/>
 		<AboutHero/>
+		<Story/>
+		<Team/>
+		<Partners/>
+		<Feedback/>
+		<Footer/>
+		</Box>
 		</>
 	 );
 }
