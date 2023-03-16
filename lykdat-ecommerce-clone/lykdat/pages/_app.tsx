0c6fde0b3612c0368fff07fa77f4a3f0697446b7
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-03-16 14:27:23 +0100

    Delete lykdat-ecommerce-clone/lykdat directory

--- lykdat-ecommerce-clone/lykdat/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '<lykdat>/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (<ChakraProvider><Component {...pageProps}/></ChakraProvider>)
-}
