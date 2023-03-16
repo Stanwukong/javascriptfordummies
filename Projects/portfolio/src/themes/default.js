de6d09ce315643de7af6fe4a37bf29630969f0ee
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-03-16 14:23:43 +0100

    Initial Commit

---
+++ Projects/portfolio/src/themes/default.js
@@ -0,0 +1,22 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
+  },
+  // Colors for layout
+  colors: {
+    primary1: "hsl(204,23.8%,95.9%)",
+    background1: "rgb(43, 52, 103)",
+    accent1: "rgb(186, 215, 233)",
+    button: "rgb(235, 69, 95)",
+    background2: "rgb(186, 215, 233)",
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+}
